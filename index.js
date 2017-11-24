e631c07a2312a84318cffa5bdf63bfdd37184677
David J Towster <djt4xe@virginia.edu>
2017-11-24 06:08:21 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,27 @@
+function shout(input) {
+  return input.toUpperCase()
+}
+
+function whisper(input) {
+  return input.toLowerCase()
+}
+
+function logShout(input) {
+  console.log(input.toUpperCase())
+}
+
+function logWhisper(input) {
+  console.log(input.toLowerCase())
+}
+
+function sayHiToGrandma(string) {
+  if (string === lowercase) {
+    return 'I can\'t hear you!'
+  }
+  else if (string === uppercase) {
+    return 'YES INDEED!'
+  }
+  else if (string === 'I love you, Grandma.') {
+    return 'I love you, too.'
+  }
+}
